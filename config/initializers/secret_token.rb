b94bd35588c1addc043578fb760678b4f1637289
Edmund Mai <edmundmai@gmail.com>
2013-03-09 11:22:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+X20130309Flickr::Application.config.secret_token = 'fa96dd55fc6a94ac3e4f570decd5998bea555858dd561c4ae186ae2820b5c58170923b2907be1e1494f6d8b2ef3eef49c4467616724a02df4a7b0c0e4895e4f8'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+X20130309Flickr::Application.routes.draw do
+
+  root :to => 'home#index'
+end
